1d109aa8a7e1de252f2395647a2ae5ce7e6f0110
Jon Arne Viksås <hackjack@tutanota.com>
2021-07-12 14:53:38 +0200

    added introduction to the parts in the script

--- install/repos/own/install/install.bash
+++ install/repos/own/install/install.bash
@@ -9,6 +9,14 @@
 ###############################################
 #Installing ubuntu 20 config
 
+function intro(){
+  echo '#########################################################################################################'
+  echo "#########     $1"
+  echo '#########################################################################################################'
+}
+
+intro "Setting environment variables"
+
 EMAIL='hackjack@tutanota.com'
 USR='hackjack'
 GITHUBPROFILE='craftsmandigital'
@@ -27,13 +35,12 @@ GITHUBPROFILE='craftsmandigital'
 
 
 
-#Initializing git
+intro "Config git username and email"
 git config --global user.email $EMAIL
 git config --global user.name $USR
 
 
-
-# keygen stuff, Credentials for github
+intro "keygen stuff, Credentials for github"
 #https://docs.github.com/en/github/authenticating-to-github/connecting-to-github-with-ssh/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent
 echo "Enter password for SSH key encryption"
 ssh-keygen -t ed25519 -C $EMAIL
@@ -44,29 +51,26 @@ cat ~/.ssh/id_ed25519.pub | clip.exe
 wslview https://github.com/settings/keys
 read -p "The ssh key is copied to the clipboard\nPaste ssh key when creating a new ssh key in the github web page\nSave ssh key\nPress any key to resume ..."
 
-
-# install stow
+intro "install stow"
 # https://zoomadmin.com/HowToInstall/UbuntuPackage/stow
 sudo apt-get update -y
 sudo apt-get install -y stow
 
 
-
-# Downoload my dotfiles
+intro 'Downoload my dotfiles from git'
+echo 'First time on git. Please copy and fill in the fingerprint prashe'
 git clone git@github.com:$GITHUBPROFILE/dotfiles.git ~/stow
-# Spread dotfiles to the right places
+intro 'Spread dotfiles to the right placeswth stow'
 # Here you can read more about stow
 # https://paramdeo.com//blog/managing-dotfiles-with-git-and-gnu-stow
 cd ~/stow
 stow */ # Everything (the '/' ignores the README)
 cd ~
 
-
+intro 'Install and Setup Zsh in Ubuntu 20.04'
 # How to Install and Setup Zsh in Ubuntu 20.04
 # https://www.tecmint.com/install-zsh-in-ubuntu/
 sudo apt install zsh -y
-
-
 # make ZSH our default shell. Use the “chsh” command with '-s' flag to switch the default shell for the user.
 # https://www.tecmint.com/install-zsh-in-ubuntu/
 chsh -s $(which zsh)
@@ -74,10 +78,9 @@ chsh -s $(which zsh)
 # moving my dotfiles from ~ to .config. This was done when I instlled dotfiles
 # telling the system that my startup file is moved to .config
 echo 'export ZDOTDIR=$HOME/.config/zsh' > $HOME/.zshenv #all config in .config folder
-rm $HOME/.zshrc # Remove startup from installation
+# rm $HOME/.zshrc # Remove startup from installation
 
-
-# install program that is dependent on ChristianChiarulli zsh config witch I have copied
+intro 'install program that is dependent on ChristianChiarulli zsh config witch I have copied zoxide'
 # https://github.com/ChristianChiarulli/Machfiles/tree/master/zsh/.config/zsh
 # https://github.com/ajeetdsouza/zoxide
 # curl -sS https://webinstall.dev/zoxide | bash
@@ -93,8 +96,7 @@ chmod +x /tmp/zoxide.bash
 
 
 
-
-# Setting Up Neovim on WSL2 on Ubuntu
+intro 'Setting Up Neovim on WSL2 on Ubuntu'
 # https://evancalz.medium.com/setting-up-neovim-on-wsl2-bf634cac435f
 # add the repo
 sudo add-apt-repository -y ppa:neovim-ppa/unstable
